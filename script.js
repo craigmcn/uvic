604a39a708ee18e14a47ed65dbe285881b5eb40e
Craig McNaughton <therealcraigmcn@gmail.com>
2015-03-21 22:14:46 -0600

    Update script.js
    Added the current image index and moved the previous and next index calculations to the loadImage function

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 (function($) {
 
 	$('.gallery').each(function(i, el){
-		var $el = $(el);
+		var $gallery = $('.gallery'),
+			$el = $(el);
 		$el.on('click', function(e){
 			e.preventDefault();
-			loadImage($el.attr('href'), $el.attr('title'));
+			loadImage($gallery.index(el), $el.attr('href'), $el.attr('title'));
 		});
 	});
 
@@ -13,19 +14,19 @@
 		$el.on('click', function(e){
 			e.preventDefault();
 			var $image = $('.gallery:eq('+$el.data('index')+')'),
-				index = parseInt($el.data('index')),
-				gallery_count = $('.gallery').length-1,
-				image_prev = ((index-1) < 0) ? gallery_count : (index-1),
-				image_next = ((index+1) > gallery_count) ? 0 : (index+1);
-			$('#rotator-button-prev').data('index', image_prev);
-			$('#rotator-button-next').data('index', image_next);
-			loadImage($image.attr('href'), $image.attr('title'));
+				index = parseInt($el.data('index'));
+			loadImage(index, $image.attr('href'), $image.attr('title'));
 		});
 	});
 
-	function loadImage(src, text) {
+	function loadImage(index, src, text) {
+		var gallery_count = $('.gallery').length-1,
+			image_prev = ((index-1) < 0) ? gallery_count : (index-1),
+			image_next = ((index+1) > gallery_count) ? 0 : (index+1);
 		$('#rotator-image').attr('src', src);
 		$('#rotator-text').html(text);
+		$('#rotator-button-prev').data('index', image_prev);
+		$('#rotator-button-next').data('index', image_next);
 	}
 
 })(jQuery);
